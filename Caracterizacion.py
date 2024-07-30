94a2ded9ba068b9378a596370664254659229868
Maria_Cruz <64433275+miakreira08@users.noreply.github.com>
2024-07-30 14:34:33 -0500

    Update Caracterizacion.py

--- Caracterizacion.py
+++ Caracterizacion.py
@@ -36,6 +36,7 @@ base=pd.read_excel("https://raw.githubusercontent.com/miakreira08/IGAC/main/base
 bins = [0, 0.5, 0.8, 1]
 labels = ['Menor a 50%', 'Entre 50% y 80%', 'Mayor al 80%']
 base['categoria_tasa'] = pd.cut(base['tasa_rural'], bins=bins, labels=labels, right=False)
+st.table(base.head(3))
 gdf=gpd.GeoDataFrame(base,geometry='geometry')
 gdf.set_crs("EPSG:4326", inplace=True)
 
